5cd6e4f1cb74e5e09a70b131acac65d72eb1893b
lud <ludovic@demblans.com>
2024-02-12 08:44:12 +0100

    2.1 ok

--- src/first.rs
+++ src/first.rs
@@ -2,13 +2,14 @@ pub struct List {
   head: Link,
 }
 
+enum Link {
+  Empty,
+  // Try with just Node, it compiles
+  More(Box<Node>),
+}
+
 struct Node {
   elem: i32,
   next: Link,
 }
 
-pub enum Link {
-  Empty,
-  // Try with just Node, it compiles
-  More(Box<Node>),
-}
